cb4c06a6f63e930b18d2cc3e86eed095e61dede1
whth <whvod222@naver.com>
2025-11-06 06:20:16 +0900

    re

--- pointcloud/make_pointcloud/view_pointcloud.py
+++ pointcloud/make_pointcloud/view_pointcloud.py
@@ -1,7 +1,7 @@
 import numpy as np
 import open3d as o3d
 
-ply_path = "AIRKON/pointcloud/cloud_rgb_ply/cloud_rgb_1.ply"
+ply_path = "./out_visible/visible_20251104_003127.ply"
 pcd = o3d.io.read_point_cloud(ply_path)
 
 # 보기용 좌우 반전 행렬 (Y축 부호 반전)
